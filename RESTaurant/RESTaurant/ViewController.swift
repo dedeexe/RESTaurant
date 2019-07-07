c6496f947358840ca01dfa4ff338e50fd18b4257
dede.exe <dede.exe@gmail.com>
2019-07-07 18:23:52 -0300

    Once upon a time...

---
+++ RESTaurant/RESTaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESTaurant
+//
+//  Created by dede.exe on 07/07/19.
+//  Copyright © 2019 dede.exe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RESTaurant/RESTaurantTests/RESTaurantTests.swift
@@ -0,0 +1,34 @@
+//
+//  RESTaurantTests.swift
+//  RESTaurantTests
+//
+//  Created by dede.exe on 07/07/19.
+//  Copyright © 2019 dede.exe. All rights reserved.
+//
+
+import XCTest
+@testable import RESTaurant
+
+class RESTaurantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
